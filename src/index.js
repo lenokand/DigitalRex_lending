c2d1161225f1c9f54fdb156056dfcfe50b2720c1
unknown <shtanko1991@gmail.com>
2021-04-01 17:56:57 +0300

    main

--- src/index.js
+++ src/index.js
@@ -18,3 +18,32 @@ var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
 var scene = document.getElementById('scene2');
 var parallax = new Parallax(scene2);
+var scene = document.getElementById('scene3');
+var parallax = new Parallax(scene3);
+var scene = document.getElementById('scene4');
+var parallax = new Parallax(scene4);
+var scene = document.getElementById('scene5');
+var parallax = new Parallax(scene5);
+var scene = document.getElementById('scene6');
+var parallax = new Parallax(scene6);
+var scene = document.getElementById('scene7');
+var parallax = new Parallax(scene7);
+
+
+let text = document.getElementById("contentSpoiler");
+let link = document.getElementById("linkSpoiler");
+
+link.addEventListener('click', function(e){
+    e.preventDefault();
+       if (text.style.display == "block") {
+        text.style.display = "none";
+        link.innerHTML = "подробнее..";
+    } else {
+        text.style.display = "block";
+        link.innerHTML = "скрыть..";
+    }
+
+})
+
+
+
