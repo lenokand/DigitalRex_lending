459fa35f0f46e0cf60ded39619a5b18814922157
unknown <shtanko1991@gmail.com>
2021-04-21 12:52:45 +0300

    result

--- src/index.js
+++ src/index.js
@@ -20,3 +20,12 @@ const toggleMenu = () => {
 burger.addEventListener('click', toggleMenu)
 shadow.addEventListener('click', toggleMenu)
 
+
+
+
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
+var scene2 = document.getElementById('scene2');
+var parallax = new Parallax(scene2);
+var scene3 = document.getElementById('scene3');
+var parallax = new Parallax(scene3);
